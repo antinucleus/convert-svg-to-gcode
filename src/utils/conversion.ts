c136eb98adfc4f93b647da3d8fa9aafbb4d39b31
antinucleus <antinucleuss@gmail.com>
2024-03-23 17:30:16 +0300

    fix(conversion): add missed aspetRatio calculation, add fill property.

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -4,7 +4,8 @@ import { Options, Units } from "../types";
 const calcualteConversion = (
   config: Options,
   fileW: string | number,
-  fileH: string | number
+  fileH: string | number,
+  fill = false
 ) => {
   let svgFileUnit: Units = "px"; // if unit is not provided, default svg unit is px
 
@@ -16,7 +17,7 @@ const calcualteConversion = (
   }
 
   /* To-do
-  get svgScale value from svg file, check if it will be multiplied with currentSize before aspect ratio calculation
+  1 - get svgScale value from svg file, check if it will be multiplied with currentSize before aspect ratio calculation
   */
 
   const conversion: number = UnitConversion[config.unit as string][svgFileUnit];
@@ -27,8 +28,8 @@ const calcualteConversion = (
   const targetSize = { w: config.width, h: config.height };
   const currentSize = { w: fileW, h: fileH };
   const aspectRatio = currentSize.w / currentSize.h;
-  const svgScaleValue = { x: 1, y: 1 };
   const multiplier = { mw: 1, mh: 1 };
+  const svgScaleValue = { x: 0.1, y: 0.1 };
 
   if (!targetSize.w) {
     targetSize.w = targetSize.h * aspectRatio;
@@ -36,11 +37,25 @@ const calcualteConversion = (
   if (!targetSize.h) {
     targetSize.h = targetSize.w / aspectRatio;
   }
-  if (currentSize.w > targetSize.w) {
+
+  const w2 = targetSize.h * aspectRatio;
+  const h2 = targetSize.w / aspectRatio;
+
+  if (fill) {
     multiplier.mw = targetSize.w / currentSize.w;
-  }
-  if (currentSize.h > targetSize.h) {
     multiplier.mh = targetSize.h / currentSize.h;
+  } else {
+    if (currentSize.w > targetSize.w || currentSize.h > targetSize.h) {
+      if (w2 <= targetSize.w) {
+        multiplier.mw = w2 / currentSize.w;
+        multiplier.mh = targetSize.h / currentSize.h;
+      }
+
+      if (h2 <= targetSize.h) {
+        multiplier.mh = h2 / currentSize.h;
+        multiplier.mw = targetSize.w / currentSize.w;
+      }
+    }
   }
 
   return { conversion, multiplier, svgScaleValue };
